de601c3b99bc9277cecf752195f696f3de24479e
jamie7533 <lee.jamie.july@gmail.com>
2023-02-07 20:50:57 -0600

    player can sprint

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
 
     // A field editable from inside Unity with a default value of 5
     public float speed = 5.0f;
+    public float sprintSpeed = 10.0f;
 
     // How much will the player slide on the ground
     // The lower the value, the greater distance the user will slide
@@ -34,29 +35,18 @@ public class PlayerMovement : MonoBehaviour
         init_y = transform.position.y;
     }
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        // This will detect forward and backward movement
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
-
-        // This will detect sideways movement
-        verticalMovement = Input.GetAxisRaw("Vertical");
-
-        
-
-        // Calculate the direction to move the player
-        Vector3 movementDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
-        
-        // Move the player
-        rb.AddForce(movementDirection * speed, ForceMode.Force);
-        // Apply drag
-        rb.drag = drag;
-    }*/
-
     private void Update()
     {
-  
+        
+        float currentSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
         Vector3 total_v = new Vector3(0, 0, 0);
         if(Input.GetKey(KeyCode.W))
         {
@@ -74,11 +64,11 @@ public class PlayerMovement : MonoBehaviour
         {
             total_v += transform.right;
         }
-        total_v = Vector3.Scale(total_v, yLock);
+        total_v.y = 0; // = Vector3.Scale(total_v, yLock);
         total_v.Normalize();
-        rb.velocity = speed * total_v;
+        rb.velocity = currentSpeed * total_v;
 
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space) && transform.position.y <= init_y)
         {
             rb.AddForce(new Vector3(0, jump, 0));
         }
